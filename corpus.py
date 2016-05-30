677c6fb82ff548cab70997596201d1d6ef22e1fd
Jonathan Reeve <jon.reeve@gmail.com>
2016-05-30 18:43:21 -0400

    probably a line break thing

--- corpus.py
+++ corpus.py
@@ -1,31 +1,31 @@
 import click
 import yaml
 import pandas
-from tabulate import tabulate
+import sh
 
 class Config(object):
     def __init__(self, config='config.yaml'):
-        """Gets the config file. Unless the user specifies something, 
+        """Gets the config file. Unless the user specifies something,
         this will be in the current directory."""
-        try: 
+        try:
             self.config = open(config).read()
-        except: 
+        except:
             raise click.ClickException("Couldn't find the config file!")
-        try: 
+        try:
             configDict = yaml.safe_load(self.config)
             self.listFilename = configDict['corpuslist']
             self.downloadTo = configDict['downloadTo']
-        except: 
+        except:
             raise click.ClickException("Couldn't parse the config file. Is it in the right format?")
 
 @click.group()
 @click.version_option('0.1')
 @click.pass_context
 def cli(ctx):
-    """Corpus is a command line tool that lists and downloads textual corpora. 
+    """Corpus is a command line tool that lists and downloads textual corpora.
 
     This tool was originally created for use in the Digital Humanities
-    toolbox called DHBox. 
+    toolbox called DHBox.
     """
     # Create a config object and remember it as as the context object.  From
     # this point onwards other commands can refer to it by using the
@@ -33,8 +33,8 @@ def cli(ctx):
     ctx.obj = Config()
 
     # This allows Pandas to display at the current terminal width.
-    pandas.set_option('display.width', None) 
-    
+    pandas.set_option('display.width', None)
+
 @cli.command()
 @click.option('--centuries', help='Comma-separated list of centuries to display, e.g. 16th,17th.')
 @click.option('--categories', help='Comma-separated list of categories to display, e.g. literature,classics.')
@@ -46,39 +46,41 @@ def list(ctx, centuries, categories):
     corpuslist = readCorpusList()
     fields = ['title', 'centuries', 'categories']
     showCorpusList(corpuslist, fields, centuries, categories)
-    
+
 @click.pass_obj
-def readCorpusList(ctx): 
+def readCorpusList(ctx):
     """Reads the corpus list from corpus-list.yaml (or other file specified in the config).
     Returns a pandas data frame.
     """
-    try: 
+    try:
         corpusList = open(ctx.listFilename).read()
-    except: 
+    except:
         raise click.ClickException("Couldn't read the corpus list from %s." % ctx.listFilename)
-    try: 
+    try:
         corpusListDict = yaml.safe_load(corpusList)
+        corpusDict = {x['shortname']: x for x in corpusListDict}
+        print(corpusDict)
         corpusListDF = pandas.DataFrame(corpusListDict).set_index('shortname')
-    except: 
+    except:
         raise click.ClickException("Couldn't parse the corpus list from %s. Is it in the right format?" % ctx.listFilename)
 
     return corpusListDF
 
-def filterCorpusList(corpuslist, field, values): 
+def filterCorpusList(corpuslist, field, values):
     values = values.split(',')
     values = ('|').join(values) # Pandas format for OR statements is like "16th|17th"
     corpuslist = corpuslist[corpuslist[field].str.contains(values, na=False)]
     return corpuslist
-    
+
 def showCorpusList(corpusListDF, fields, centuries=None, categories=None):
 
-    # Filter by default fields. 
+    # Filter by default fields.
     table = corpusListDF[fields]
 
-    if centuries is not None: 
+    if centuries is not None:
         table = filterCorpusList(table, 'centuries', centuries)
 
-    if categories is not None: 
+    if categories is not None:
         table = filterCorpusList(table, 'categories', categories)
 
     print(table)
@@ -91,33 +93,33 @@ def showCorpusList(corpusListDF, fields, centuries=None, categories=None):
 def download(ctx, shortname, destination, markup=None):
     """Downloads a corpus.
 
-    This will download the corpus with the given shortname into the 
-    download destination. If the download destination is not provided,  
-    this will automatically use the default download location, given 
+    This will download the corpus with the given shortname into the
+    download destination. If the download destination is not provided,
+    this will automatically use the default download location, given
     by the config file.
     """
 
-    # Check to make sure the requested corpus exists. 
+    # Check to make sure the requested corpus exists.
     corpusList = readCorpusList()
-    if shortname not in corpusList.index.tolist(): 
+    if shortname not in corpusList.index.tolist():
         raise click.ClickException("Couldn't find the specified corpus. Are you sure you have the right shortname?")
-    
+
     if destination is None:
         destination = ctx.downloadTo
 
     corpus = corpusList.ix[shortname]
-    
+
     print(corpus)
 
-    text = corpus.text 
+    text = corpus.text
 
     # A convoluted way to check the type of the corpus text item,
-    # but since we've redefined `list` above, we can't do `if type(corpus) is list`.  
-    if type(text) == type([]): 
-        # This means we have more than one text type, and we need to disambiguate. 
+    # but since we've redefined `list` above, we can't do `if type(corpus) is list`.
+    if type(text) == type([]):
+        # This means we have more than one text type, and we need to disambiguate.
 
-        # Check to see whether the user has already specified the text. 
-        if markup is None:  
+        # Check to see whether the user has already specified the text.
+        if markup is None:
             # If the user hasn't specified a text...
             markupTypes = ', '.join([textType['markup'] for textType in text])
             raise click.ClickException('There are %s text types in this corpus: %s. Please specify which one you want with the --markup flag.' % (len(text), markupTypes))
