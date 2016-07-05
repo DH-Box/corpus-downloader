00478e5bd5f001f3981312ee5c05b4aa83fc1b97
Jonathan Reeve <jon.reeve@gmail.com>
2016-07-05 13:11:56 -0400

    better setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,28 @@
-from setuptools import setup
+import sys
+from distutils.core import setup
+
+if sys.version_info[0] < 3:
+    version = str(sys.version_info[0]) + '.' + str(sys.version_info[1])
+    sys.exit("""
+    Sorry! Your Python version is %s, but this program requires at least
+    Python 3. Please upgrade your Python installation, or try using pip3
+    instead of pip.""" % version)
 
 setup(
     name='corpus-downloader',
-    version='0.1',
+    packages = ['corpus'], # this must be the same as the name above
     py_modules=['corpus'],
+    version='0.1.0',
+    description = 'A downloader for textual corpora, for use in digital humanities, corpus linguistics, and natural language processing.',
+    author = 'Jonathan Reeve',
+    author_email = 'jon.reeve@gmail.com',
     include_package_data=True,
+    keywords = ['nlp', 'text-analysis', 'corpora'],
     install_requires=[
-        'click',
+        'click','pandas','pyyaml','sh'
     ],
     entry_points='''
         [console_scripts]
-        corpus=corpus:cli
+        corpus=corpus.corpus:cli
     ''',
 )
