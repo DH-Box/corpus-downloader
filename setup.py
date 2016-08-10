7aa0fcf4f6ab91f9d9e353a479c6a217888501f4
Jonathan Reeve <jon.reeve@gmail.com>
2016-08-10 14:26:57 +0900

    increment version

--- setup.py
+++ setup.py
@@ -12,12 +12,12 @@ setup(
     name='corpus-downloader',
     packages = ['corpus'], # this must be the same as the name above
     py_modules=['corpus'],
-    version='0.1.4',
+    version='0.1.5',
     description = 'A downloader for textual corpora, for use in digital humanities, corpus linguistics, and natural language processing.',
     author = 'Jonathan Reeve',
     author_email = 'jon.reeve@gmail.com',
     url = 'https://github.com/DH-Box/corpus-downloader',
-    download_url = 'https://github.com/DH-Box/corpus-downloader/tarball/0.1.4',
+    download_url = 'https://github.com/DH-Box/corpus-downloader/tarball/0.1.5',
     include_package_data=True,
     package_data = { 
         'corpus': ['corpus-list/corpus-list.yaml']
